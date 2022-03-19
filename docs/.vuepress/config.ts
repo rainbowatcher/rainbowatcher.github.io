43430356e97c73653001f622ef37211392d9a33a
吴鸿博 <rainbow-w@qq.com>
2022-03-19 16:18:10 +0800

    add

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,32 @@
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  title: "看彩虹的人",
+  lang: "zh-CN",
+  description: "小彩虹的个人主页",
+  themeConfig: {
+    // logo: '/logo.ico',
+    logoDark: "",
+    navbar: [
+      { text: "foo", link: "/" },
+      { text: "bar", link: "/" },
+    ],
+    repo: "https://github.com/with-rainbow/with-rainbow.github.io",
+    // repoLabel: '项目'
+    // 编辑页面配置
+    docsRepo: "https://github.com/with-rainbow/with-rainbow.github.io",
+    docsBranch: "main",
+    docsDir: "docs",
+    editLinkPattern: ":repo/edit/:branch/:path",
+    editLinkText: "编辑页面",
+    lastUpdated: true,
+    lastUpdatedText: "最后更新",
+    contributors: true,
+    contributorsText: "作者",
+    backToHome: "返回主页",
+    toggleDarkMode: "切换主题",
+    toggleSidebar: "switch",
+    notFound: ["页面走丢了", "没发现你要的页面", "没找到呢"],
+  },
+  plugins: [],
+});
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+title: 梅花羊
+heroText: 羊圈
+tagline: 人生无常，大肠包小肠
+actions:
+    - text: 笔记
+      link: /notes/
+---
+
