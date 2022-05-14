c160216b3d74bace3a2aaab7af15b8d553d3bafc
吴鸿博 <rainbow-w@qq.com>
2022-05-14 22:23:52 +0800

    add: 添加markdown语法测试

---
+++ .vuepress/theme.ts
@@ -0,0 +1,101 @@
+import { GungnirThemeOptions } from "vuepress-theme-gungnir";
+
+const themeConfig: Partial<GungnirThemeOptions> = {
+  // logo: '/logo.ico',
+  // logoDark: "",
+  // navbar: [{ text: "笔记", link: "https://with-rainbow.github.io/notes" }],
+  // repo: "https://github.com/with-rainbow/with-rainbow.github.io",
+  // // repoLabel: '项目'
+  // // 编辑页面配置
+  // docsRepo: "https://github.com/with-rainbow/with-rainbow.github.io",
+  // docsBranch: "main",
+  // docsDir: "docs",
+  // editLinkPattern: ":repo/edit/:branch/:path",
+  // editLinkText: "编辑页面",
+  // lastUpdated: true,
+  // lastUpdatedText: "最后更新",
+  // contributors: true,
+  // contributorsText: "作者",
+  // backToHome: "返回主页",
+  // toggleDarkMode: "切换主题",
+  // toggleSidebar: "switch",
+  // notFound: ["页面走丢了", "没发现你要的页面", "没找到呢"],
+  mermaid: true,
+  katex: true,
+  // navbarTitle: "test",
+  personalInfo: {
+    name: "with-rainbow",
+    avatar: "/img/avatar.jpg",
+    description: "BigData engineer, love with programing",
+    sns: {
+      github: "with-rainbow", // Github
+      email: "renovamenzxh@gmail.com", // 邮箱
+      rss: "/rss.xml", // RSS 文件的链接
+    },
+  },
+  // 首页图片
+  homeHeaderImages: [
+    {
+      path: "/img/home-bg/2.jpg",
+      mask: "rgb(100, 100, 100, .1)",
+    },
+    {
+      path: "/img/home-bg/4.jpg",
+      mask: "rgb(100, 100, 100, .1)",
+    },
+    {
+      path: "/img/home-bg/5.jpg",
+      mask: "rgb(100, 100, 100, .1)",
+    },
+    {
+      path: "/img/home-bg/6.jpg",
+      mask: "rgb(100, 100, 100, .1)",
+    },
+  ],
+  pages: {
+    // 标签页配置
+    tags: {
+      title: "标签",
+      // subtitle: "吼哇朋友们，这是标签页",
+      bgImage: {
+        path: "/img/pages/tags.jpg",
+        mask: "rgba(211, 206, 237, .7)",
+      },
+    },
+    // 链接页配置
+    links: {
+      // subtitle: "吼哇朋友们，这是链接页",
+      bgImage: {
+        path: "/img/pages/links.jpg",
+        mask: "rgba(64, 118, 190, 0.5)",
+      },
+    },
+  },
+  navbar: [
+    {
+      text: "首页",
+      link: "/",
+    },
+    {
+      text: "标签",
+      link: "/tags/",
+    },
+    {
+      text: "链接",
+      link: "/links/",
+    },
+    {
+      text: "Github",
+      link: "http://github.com/with-rainbow",
+    },
+  ],
+  // 页脚配置
+  footer: `
+  &copy; <a href="https://github.com/Renovamen" target="_blank">Renovamen</a> 2018-2022
+  <br>
+  Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
+  <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
+`,
+};
+
+export default themeConfig;
---
+++ posts/2022/static-site-generator.md
@@ -0,0 +1,34 @@
+---
+layout: Post
+title: 静态站点生成器对比
+subtitle: 对比js、go、rust、ruby等语言下流行的静态站点生成器的优缺点
+headerImage: /img/2022/static-site-generator/ssg-tools.jpg
+useHeaderImage: true
+headerMask: rgba(0, 0, 0, .4)
+date: 2022-05-14
+permalinkPattern: /post/:year/:month/:day/:slug/
+hide: true
+tags:
+  - SSG
+---
+
+测试文本
+
+<!-- more -->
+
+## VuePress2
+
+基于 Vue3 和 Vite 的静态站点生成工具。
+
+缺点：
+
+目前还处于 Beta 版本，社区开源的主题和插件还不够丰富。
+
+优点：
+
+- 基于 Vue3 和 Vite。
+- 提供默认主题
+
+## Zola
+
+是基于 Rust 语言的静态模板框架，需要学 tera 模板语法。
