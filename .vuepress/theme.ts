f69a961a09b307d3cfeb67456aeaccd61332d2af
吴鸿博 <rainbow-w@qq.com>
2022-05-18 11:48:50 +0800

    chore: 增加默认主题配置

--- .vuepress/theme.ts
+++ .vuepress/theme.ts
@@ -4,22 +4,22 @@ const themeConfig: Partial<GungnirThemeOptions> = {
   // logo: '/logo.ico',
   // logoDark: "",
   // navbar: [{ text: "笔记", link: "https://with-rainbow.github.io/notes" }],
-  // repo: "https://github.com/with-rainbow/with-rainbow.github.io",
-  // // repoLabel: '项目'
-  // // 编辑页面配置
-  // docsRepo: "https://github.com/with-rainbow/with-rainbow.github.io",
-  // docsBranch: "main",
-  // docsDir: "docs",
-  // editLinkPattern: ":repo/edit/:branch/:path",
-  // editLinkText: "编辑页面",
-  // lastUpdated: true,
-  // lastUpdatedText: "最后更新",
+  repo: "https://github.com/with-rainbow/with-rainbow.github.io",
+  repoLabel: '项目',
+  // 编辑页面配置
+  docsRepo: "https://github.com/with-rainbow/with-rainbow.github.io",
+  docsBranch: "main",
+  // docsDir: "posts",
+  editLinkPattern: ":repo/edit/:branch/:path",
+  editLinkText: "编辑页面",
+  lastUpdated: true,
+  lastUpdatedText: "最后更新",
   // contributors: true,
   // contributorsText: "作者",
-  // backToHome: "返回主页",
-  // toggleDarkMode: "切换主题",
+  backToHome: "返回主页",
+  toggleDarkMode: "切换主题",
   // toggleSidebar: "switch",
-  // notFound: ["页面走丢了", "没发现你要的页面", "没找到呢"],
+  notFound: ["页面走丢了", "没发现你要的页面", "没找到呢"],
   mermaid: true,
   katex: true,
   // navbarTitle: "test",
@@ -73,10 +73,6 @@ const themeConfig: Partial<GungnirThemeOptions> = {
   },
   navbar: [
     {
-      text: "首页",
-      link: "/",
-    },
-    {
       text: "标签",
       link: "/tags/",
     },
@@ -85,13 +81,13 @@ const themeConfig: Partial<GungnirThemeOptions> = {
       link: "/links/",
     },
     {
-      text: "Github",
-      link: "http://github.com/with-rainbow",
+      text: "笔记",
+      link: "https://notes-with-rainbow.vercel.app",
     },
   ],
   // 页脚配置
   footer: `
-  &copy; <a href="https://github.com/Renovamen" target="_blank">Renovamen</a> 2018-2022
+  &copy; <a href="https://github.com/with-rainbow" target="_blank">with-rainbow</a> 2018-2022
   <br>
   Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
   <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
