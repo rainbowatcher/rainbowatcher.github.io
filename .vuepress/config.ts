bded190b1e7ca629ab47e5cc3bef26250859e45b
吴鸿博 <rainbow-w@qq.com>
2022-05-21 23:25:45 +0800

    ✨ feat: update dependency version to lastest

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,40 +1,37 @@
-import { defineUserConfig } from "vuepress";
-import type { GungnirThemeOptions } from "vuepress-theme-gungnir";
+import { defineUserConfig, viteBundler } from "vuepress";
+import { gungnirTheme } from "vuepress-theme-gungnir";
+import katexPlugin from "@renovamen/vuepress-plugin-katex";
+import mermaidPlugin from "@renovamen/vuepress-plugin-mermaid";
+import mdPlusPlugin from "@renovamen/vuepress-plugin-md-plus";
+import giscusPlugin from "vuepress-plugin-giscus";
 import themeConfig from "./theme";
 
-export default defineUserConfig<GungnirThemeOptions>({
-  title: "Plum Sheep",
+export default defineUserConfig({
+  title: "ClubSheep",
   lang: "zh-CN",
   description: "Sheep Sheep Sheep",
-  theme: "gungnir",
-  themeConfig: themeConfig,
-  head:[
-    ["link", { rel: "icon", href: "sheep.svg" }],
-  ],
+  theme: gungnirTheme(themeConfig),
+  head: [["link", { rel: "icon", href: "sheep.svg" }]],
+  markdown: {
+    code: {
+      lineNumbers: true,
+    },
+  },
   plugins: [
-    ["@renovamen/vuepress-plugin-katex"],
-    [
-      "@renovamen/vuepress-plugin-mermaid",
-      {
-        token: "mermaid",
-        theme: "forest",
-      },
-    ],
-    [
-      "@renovamen/vuepress-plugin-md-plus",
-      {
-        all: true,
-      },
-    ],
+    katexPlugin(),
+    mermaidPlugin({
+      token: "mermaid",
+      theme: "forest",
+    }),
+    mdPlusPlugin({
+      all: true,
+    }),
     // https://giscus.app/zh-CN
-    [
-      "vuepress-plugin-giscus",
-      {
-        repo: "with-rainbow/with-rainbow.github.io",
-        repoId: "R_kgDOHBYFAg",
-        category: "Announcements",
-        categoryId: "DIC_kwDOHBYFAs4CPGQS",
-      },
-    ],
+    giscusPlugin({
+      repo: "with-rainbow/with-rainbow.github.io",
+      repoId: "R_kgDOHBYFAg",
+      category: "Announcements",
+      categoryId: "DIC_kwDOHBYFAs4CPGQS",
+    }),
   ],
 });
--- .vuepress/theme.ts
+++ .vuepress/theme.ts
@@ -1,28 +1,21 @@
 import { GungnirThemeOptions } from "vuepress-theme-gungnir";
 
 const themeConfig: Partial<GungnirThemeOptions> = {
-  // logo: '/logo.ico',
-  // logoDark: "",
-  // navbar: [{ text: "笔记", link: "https://with-rainbow.github.io/notes" }],
   repo: "https://github.com/with-rainbow/with-rainbow.github.io",
   repoLabel: '项目',
   // 编辑页面配置
   docsRepo: "https://github.com/with-rainbow/with-rainbow.github.io",
   docsBranch: "main",
-  // docsDir: "posts",
   editLinkPattern: ":repo/edit/:branch/:path",
   editLinkText: "编辑页面",
+  // 文章底部连接文字配置
   lastUpdated: true,
   lastUpdatedText: "最后更新",
-  // contributors: true,
-  // contributorsText: "作者",
   backToHome: "返回主页",
   toggleDarkMode: "切换主题",
-  // toggleSidebar: "switch",
   notFound: ["页面走丢了", "没发现你要的页面", "没找到呢"],
   mermaid: true,
   katex: true,
-  // navbarTitle: "test",
   personalInfo: {
     name: "with-rainbow",
     avatar: "/img/avatar.jpg",
