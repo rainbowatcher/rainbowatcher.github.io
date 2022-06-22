b79ad75fb0b7c135cb73a1d5d84f777f025d7884
rainbowatcher <rainbow-w@qq.com>
2022-06-22 12:47:56 +0800

    add: oh-vue-icons support

---
+++ .vuepress/client.ts
@@ -0,0 +1,7 @@
+import { defineClientConfig } from "@vuepress/client";
+import { addIcons } from "oh-vue-icons";
+
+import { RiBookMarkFill } from "oh-vue-icons/icons";
+addIcons(RiBookMarkFill);
+
+export default defineClientConfig({});
