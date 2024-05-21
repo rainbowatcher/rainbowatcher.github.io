82335f25ae907f2bd3a35023aefe3841ab81ec2c
rainbowatcher <rainbow-w@qq.com>
2024-05-21 11:25:51 +0800

    feat: update to eslint v9 and use @rainbowatcher/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "@rainbowatcher/eslint-config";
+
+export default defineConfig({
+    gitignore: true,
+    typescript: true,
+    vue: true,
+    style: true,
+    json: true,
+    css: true,
+    markdown: true,
+    unocss: true,
+    yaml: true,
+});
