ae5a02a0fa8501c44f695b2524506e23e0042627
rainbowatcher <rainbow-w@qq.com>
2024-06-03 17:15:53 +0800

    style: add space between content and inline code

--- src/components/content/ProseCodeInline.vue
+++ src/components/content/ProseCodeInline.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <code class="rounded-md bg-popover px-1.5 py.5 text-sm c-popover-foreground font-mono ring-1 ring-border">
+    <code class="mx-2px rounded-md bg-popover px-1.5 py.5 text-sm c-popover-foreground font-mono ring-1 ring-border">
         <slot />
     </code>
 </template>
