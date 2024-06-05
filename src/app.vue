b8248fee505d2e1d60266926a0e5e93965c7a6a9
rainbowatcher <rainbow-w@qq.com>
2024-06-05 23:14:22 +0800

    fix: follow nuxt/#12735 fix layout error

--- src/app.vue
+++ src/app.vue
@@ -6,16 +6,18 @@ useHead(globalHead)
 </script>
 
 <template>
-    <NuxtLayout>
-        <NuxtPage
-            class="transition duration-150"
-            :transition="{
-                enterActiveClass: 'ease-in-out',
-                enterFromClass: 'opacity-20 blur-sm translate-y-2',
-                leaveActiveClass: 'ease-in-out',
-                leaveToClass: 'opacity-20 blur-sm translate-y-2',
-                mode: 'out-in',
-            }"
-        />
-    </NuxtLayout>
+    <div class="#12735 flex flex-1 flex-col min-h-dvh">
+        <NuxtLayout>
+            <NuxtPage
+                class="transition duration-150"
+                :transition="{
+                    enterActiveClass: 'ease-in-out',
+                    enterFromClass: 'opacity-20 blur-sm translate-y-2',
+                    leaveActiveClass: 'ease-in-out',
+                    leaveToClass: 'opacity-20 blur-sm translate-y-2',
+                    mode: 'out-in',
+                }"
+            />
+        </NuxtLayout>
+    </div>
 </template>
