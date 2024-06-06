252e3c5e3e8f014a475adaad15d4e49df01257dd
rainbowatcher <rainbow-w@qq.com>
2024-06-07 01:40:20 +0800

    feat: add font settings in uno config

--- uno.config.ts
+++ uno.config.ts
@@ -126,8 +126,9 @@ export default defineConfig({
             },
         },
         fontFamily: {
-            mono: "Fira Code",
-            serif: "Red Hat Display",
+            mono: "\"Fira Code\", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, \"Liberation Mono\", \"Courier New\", monospace",
+            sans: "\"Red Hat Display\", \"Chinese Quotes\", Inter, ui-sans-serif, system-ui, sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\", \"Noto Color Emoji\", \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\", \"Noto Color Emoji\"",
+            serif: "ui-serif, Georgia, Cambria, \"Times New Roman\", Times, serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\", \"Noto Color Emoji\"",
         },
     },
     transformers: [
