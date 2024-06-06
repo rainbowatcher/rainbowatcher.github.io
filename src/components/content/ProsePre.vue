48da58cdf444263407483a04ad491848e9baa382
rainbowatcher <rainbow-w@qq.com>
2024-06-07 02:58:38 +0800

    style: adjust pre tag

--- src/components/content/ProsePre.vue
+++ src/components/content/ProsePre.vue
@@ -25,13 +25,13 @@ const id = useId()
     <Mermaid v-if="language === 'mermaid'" :id="id" :graph="code" />
     <template v-else>
         <div class="group code-pre relative overflow-auto rounded-lg bg-popover">
-            <span v-if="filename" class="absolute left-4.5 top-2 select-none text-sm c-neutral5">{{ filename }}</span>
-            <span class="absolute right-4.5 top-2 ml-auto select-none text-sm c-neutral5 lowercase transition-opacity duration-300 ease-in-out group-hover:(op0 blur)">{{ language }}</span>
+            <span v-if="filename" class="absolute left-6.5 top-2 select-none text-sm c-neutral5 font-sans">{{ filename }}</span>
+            <span class="absolute right-6.5 top-2 ml-auto select-none text-sm c-neutral5 font-sans lowercase transition-opacity duration-300 ease-in-out group-hover:(op0 blur)">{{ language }}</span>
             <div class="code-copy absolute right-2.5 top-2.5 b b-border rounded bg-[hsl(from_var(--muted)_h_s_l_/10%)] px-2 py1 op0 blur transition-opacity duration-300 ease-in-out active:bg-[hsl(from_var(--muted)_h_s_l_/10%)] hover:bg-[hsl(from_var(--muted)_h_s_l_/5%)] group-hover:(op100 blur-none)">
                 <div i-ph-copy c-input />
             </div>
             <pre
-                class="overflow-auto rounded-lg bg-popover p4 [&_.highlight]:(mx--4 px-4) [&>code>.line]:(block text-sm leading-6 font-mono) ![&_.highlight]:(bg-black/10 dark:bg-black/65)" :class="[$props.class, { pt8: filename }]"
+                class="overflow-auto rounded-lg bg-popover py5 [&_.highlight]:(mx--6 px-6) [&>code]:(block px-6) [&>code>.line]:(block vertical-text-bottom text-sm leading-6 font-mono) ![&_.highlight]:(bg-black/10 dark:bg-black/45)" :class="[$props.class, { pt8: filename }]"
             ><slot /></pre>
         </div>
     </template>
