431b372adc4194e6a5cfacfc4e09945502ff87a3
rainbowatcher <rainbow-w@qq.com>
2024-06-14 02:16:35 +0800

    chore: make it be client component

--- src/components/PageNav.vue
+++ src/components/PageNav.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import type { NavItem } from "@nuxt/content/types"
 
-
 type PageNavProps = {
     next?: Partial<NavItem>
     previous?: Partial<NavItem>
@@ -15,12 +14,12 @@ defineProps<PageNavProps>()
 <template>
     <div class="page-nav mt-auto flex justify-between py8 sm:(mx-[calc(5rem+3rem)] flex-row flex-nowrap) lt-sm:flex-col">
         <div v-if="previous" class="max-w-80 b-l b-l-border px8 py4">
-            <NuxtLink :to="usePermalink ? previous.permalink : previous._path" class="font-serif">
+            <NuxtLink prefetch @click="navigateTo(usePermalink ? previous?.permalink : previous?._path, { external: true })">
                 {{ useTitle ? previous.title : "Previous" }}
             </NuxtLink>
         </div>
         <div v-if="next" class="max-w-80 b-r b-r-border px8 py4 sm:mla">
-            <NuxtLink :to="usePermalink ? next.permalink : next._path" class="font-serif">
+            <NuxtLink prefetch @click="navigateTo(usePermalink ? next?.permalink : next?._path, { external: true })">
                 {{ useTitle ? next.title : "Next" }}
             </NuxtLink>
         </div>
