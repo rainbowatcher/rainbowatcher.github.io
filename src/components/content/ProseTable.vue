1cc91ee549b92557649b13ef9f862965ab167983
rainbowatcher <rainbow-w@qq.com>
2024-06-17 16:49:56 +0800

    feat: make table vertical scrollable

--- src/components/content/ProseTable.vue
+++ src/components/content/ProseTable.vue
@@ -1,5 +1,5 @@
 <template>
-    <table class="border-collapse caption-bottom table-auto">
+    <table class="max-w-100vw border-collapse caption-bottom table-auto of-auto lt-sm:(mx--6 block)">
         <slot />
     </table>
 </template>
