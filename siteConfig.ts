e4fc708bff673e5ec65a409a245b68b7c067d479
rainbowatcher <rainbow-w@qq.com>
2024-06-23 20:15:33 +0800

    chore: update site config

--- siteConfig.ts
+++ siteConfig.ts
@@ -1,42 +1,10 @@
 import type { NuxtConfig } from "nuxt/schema"
-// import type { UseHeadInput } from "unhead"
-// import type { MergeHead } from "zhead"
 
 const title = "Rainbow Watcher"
-// const description = "Portfolio for Rainbow Watcher"
 const domain = "rainbowatcher.github.io"
 const url = `https://${domain}/`
 const ogImage = "/og.webp"
-// const siteIcon = "/favicon.svg"
-// const darkSiteIcon = "/favicon-dark.svg"
-// const licenseUrl = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
-
-// const head = {
-//     link: [
-//         {
-//             href: darkSiteIcon,
-//             media: "(prefers-color-scheme: dark)",
-//             rel: "icon",
-//             type: "image/svg+xml",
-//         },
-//         {
-//             href: siteIcon,
-//             media: "(prefers-color-scheme: light)",
-//             rel: "icon",
-//             type: "image/svg+xml",
-//         },
-//         {
-//             href: licenseUrl,
-//             rel: "license",
-//         },
-//     ],
-//     meta: [
-//         { content: "#00aba9", name: "msapplication-TileColor" },
-//         { charset: "utf8" },
-//     ],
-//     title,
-// } satisfies UseHeadInput<MergeHead>
-
+const licenseUrl = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
 
 export const algolia: NuxtConfig["algolia"] = {
     apiKey: "f757c625852758ee96aaf2268959166e",
@@ -48,11 +16,12 @@ export const algolia: NuxtConfig["algolia"] = {
 
 export const app: NuxtConfig["app"] = {
     head: {
+        link: [
+            { href: licenseUrl, rel: "license" },
+        ],
         meta: [
             { content: ogImage, property: "og:image" },
-            // { content: description, property: "description" },
         ],
-        // title,
     },
 }
 
