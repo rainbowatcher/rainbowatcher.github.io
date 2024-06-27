9019f4d0cff27440c8949b10a40f6a971e513766
rainbowatcher <rainbow-w@qq.com>
2024-06-27 09:16:06 +0800

    feat: use transition for backtop component animation

--- app/components/BackTop.vue
+++ app/components/BackTop.vue
@@ -1,36 +1,36 @@
 <script lang="ts" setup>
 const { y } = useWindowScroll({ behavior: "smooth" })
-const show = ref("hidden")
+const [show, toggleShow] = useToggle(false)
 const backToTop = () => y.value = 0
 watch(y, () => {
-    if (show.value === "flex") {
+    if (show.value) {
         if (y.value < 800) {
-            setTimeout(() => show.value = "hidden", 300)
-            show.value = "flex op-0"
+            toggleShow(false)
         }
     } else {
         if (y.value >= 800) {
-            show.value = "flex"
+            toggleShow(true)
         }
-        return
     }
 })
 </script>
 
 <template>
-    <div
-        class="watermark-border fixed bottom-8dvh z-99 lg:right-12 lt-sm:right-4 md:right-6 sm:right-4 xl:right-24"
-        :class="[show]" style="--un-animate-duration:500ms" @click="backToTop"
+    <Transition
+        enter-active-class="animate-in-300 fade-in slide-in-from-bottom-10"
+        leave-active-class="animate-out-300 fade-out slide-out-to-bottom-10"
     >
-        <i class="i-ph-arrow-fat-up-fill" />
-    </div>
+        <div v-if="show" class="watermark-border fixed bottom-8dvh z-99 flex" @click="backToTop">
+            <i class="i-ph-arrow-fat-up-fill" />
+        </div>
+    </Transition>
 </template>
 
 <style lang="css">
 .watermark-border {
+    @apply lg:right-12 lt-sm:right-4 md:right-6 sm:right-4 xl:right-24;
     @apply c-background bg-neutral4/25;
-    @apply [transition-property:background-color,color,opacity] duration-500 items-center rounded-full p1.5;
-    @apply animate-in fade-in slide-in-from-bottom-10;
+    @apply [transition-property:background-color,color,opacity] duration-300 items-center rounded-full p1.5;
     @apply hover:(bg-neutral4/75 c-background before:blur-lg) hover:active:(bg-input c-background);
 }
 
