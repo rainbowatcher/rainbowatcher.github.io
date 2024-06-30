cf2de7ff28f9086690b2f264c4b45d7525a8f79d
rainbowatcher <rainbow-w@qq.com>
2024-06-30 22:39:34 +0800

    chore: move about to index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
+const md = "\
+I'm an developer and open-source maintainer that fond of adventurous exploration.\
+<br>\
+I write `Typescript`, `Rust`, `Python`, `Go`, and will learn `Swift` in the distant future,\
+and finally i don't like `Java`.\
+"
 useShowup(".show-up")
 </script>
 
 <template>
     <section class="font-mb lt-md:mx-12 md:(mx-12 w-3xl) lg:w-5xl">
-        <ContentDoc v-slot="{ doc }" path="/about" :head="false">
-            <ContentRendererMarkdown :value="doc" class="show-up" />
-        </ContentDoc>
+        <p class="show-up text-4rem">
+            Hi, I'm <strong class="bg-rainbow">Rainbow Watcher</strong>.
+        </p>
+        <MDC :value="md" class="show-up text-1.2rem" />
     </section>
 </template>
