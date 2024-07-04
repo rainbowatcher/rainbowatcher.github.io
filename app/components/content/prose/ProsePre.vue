288e80ae733fc1a0be6bf3c73bbe138a2e1db033
rainbowatcher <rainbow-w@qq.com>
2024-07-04 09:18:37 +0800

    style: adjust border radius and overflow

--- app/components/content/prose/ProsePre.vue
+++ app/components/content/prose/ProsePre.vue
@@ -34,7 +34,7 @@ const id = useId()
                     {{ language }}
                 </template>
             </span>
-            <div class="code-copy absolute right-2.5 top-2.5 hidden cursor-pointer b b-ring rounded bg-muted/10 px-2 py1 op-0 blur transition-opacity duration-300 ease-in-out active:bg-muted/10 hover:bg-muted/20 group-hover:(op-100 blur-none)" />
+            <div class="code-copy absolute right-2.5 top-2.5 hidden cursor-pointer b b-ring rounded-md bg-muted/10 px-2 py1 op-0 blur transition-opacity duration-300 ease-in-out active:bg-muted/10 hover:bg-muted/20 group-hover:(op-100 blur-none)" />
             <pre
                 class="overflow-auto py-6 [&_.highlight]:(mx--6 px-6) [&>code]:(block px-6) [&>code>.line]:(block v-text-bottom text-sm leading-6 font-mono) ![&_.highlight]:(bg-black/10 dark:bg-black/45)"
                 :class="[$props.class]"
