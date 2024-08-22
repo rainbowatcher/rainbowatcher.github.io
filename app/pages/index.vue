628d313c6aa03da448bddab07377038bdf756dbb
rainbowatcher <rainbow-w@qq.com>
2024-08-22 22:22:33 +0800

    docs: update home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,15 +3,20 @@ const md = "\
 I'm an developer and open-source maintainer that fond of adventurous exploration.\
 <br>\
 I write `Typescript`, `Rust`, `Python`, `Go`, and will learn `Swift` in the distant future,\
-and finally i don't like `Java`.\
+and i don't like `Java`.\
+<br>\
+I'm learning english no\
 "
 useShowup(".show-up")
 </script>
 
 <template>
     <section class="font-mb lt-md:mx-12 md:(mx-12 w-3xl) lg:w-5xl">
-        <p class="show-up text-4rem">
-            Hi, I'm <strong class="bg-rainbow">Rainbow Watcher</strong>.
+        <p class="show-up text-1.2rem">
+            Hi there, I'm
+        </p>
+        <p class="show-up bg-rainbow text-3rem">
+            Rainbow Watcher
         </p>
         <MDC :value="md" class="show-up text-1.2rem" />
     </section>
