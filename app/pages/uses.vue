db73d5c49b89daa3038ff6e81bb348bf170a6db4
rainbowatcher <rainbow-w@qq.com>
2024-09-06 17:38:59 +0800

    feat: update uses page

--- app/pages/uses.vue
+++ app/pages/uses.vue
@@ -1,82 +1,55 @@
 <script lang="ts" setup>
+const md = `
+## Editor
+
+- Editor: [VSCode](https://code.visualstudio.com/)
+- Theme: [One Dark Pro](https://github.com/Binaryify/OneDark-Pro.git)
+- Icons: [Material Design Icons](https://github.com/material-extensions/vscode-material-icon-theme)
+- Fonts:
+    - [FiraCode](https://github.com/tonsky/FiraCode)
+    - [Cascadia Code](https://github.com/microsoft/cascadia-code)
+    - [0xProto](https://github.com/0xType/0xProto)
+- Formatting & Linting: [@rainbowatcher/eslint-config](https://github.com/rainbowatcher/eslint-config)
+
+## Chrome Extensions
+
+- [Refined GitHub](https://chrome.google.com/webstore/detail/refined-github/hlepfoohegkhhmjieoechaddaejaokhf) - No-brainer must-have
+- [ViolentMonkey](https://github.com/violentmonkey/violentmonkey) - Run custom script in browser
+- [GitHub File Icons](https://github.com/homerchen19/github-file-icons) - It looks very nice!
+- [I Still Don't Care About Cookies](https://github.com/OhMyGuus/I-Still-Dont-Care-About-Cookies) - Not popup cookie notice
+- [Sala Dict](https://github.com/crimx/ext-saladict) - Dictionary
+- [Immersive Translate](https://github.com/immersive-translate/immersive-translate/)
+- [Vimium C](https://github.com/gdh1995/vimium-c) - Operate browser through keyboard
+
+## Applications
+
+- [Raycast](https://raycast.com/) - Spotlight replacement
+- [Alacritty](https://github.com/alacritty/alacritty) - OpenGL terminal emulator
+- [Localsend](https://github.com/localsend/localsend) - An open-source cross-platform alternative to AirDrop
+
+## Hardware
+
+- MacBook Pro 13 inch 2020 16GB
+- Mouse: Logitech G304
+- Keyboard: Filco Majestouch Convertible 2 TKL - Cherry MX Red
+
+## CLI
+
+- [zsh](https://zsh.org/) - Shell
+  - Config: [rainbowatcher/scripts](https://github.com/rainbowatcher/scripts)
+- [pnpm](https://pnpm.io/) - Preferred package managers, also for monorepo
+- [ni](https://github.com/antfu/ni) - Aliasing package managers
+- [taze](https://github.com/antfu/taze) - Upgrade dependencies
+- [bumpp](https://github.com/antfu/bumpp) - Bump package version version for new release
+- [tsx](https://github.com/privatenumber/tsx) - Run TypeScript files without compilation
+`
 </script>
 
 <template>
-    <div class="mx12 flex flex-row gap-4">
-        <Badge text="Tip" type="tip" />
-        <Badge text="Note" type="note" />
-        <Badge text="Warning" type="warning" />
-        <Badge text="Danger" type="danger" />
-        <Badge text="Info" type="info" />
-    </div>
-    <div class="grid grid-cols-4 m12 gap-4">
-        <div class="p4">
-            Normal ipsum dolor sit amet consectetur adipisicing elit. Officiis laborum dicta consequuntur hic beatae recusandae quisquam sed est incidunt neque?
-        </div>
-        <div class="bg-card p4 c-card-foreground">
-            Card ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolor similique ipsum obcaecati odit, laboriosam deleniti ea illum qui aperiam.
-        </div>
-        <div class="bg-primary p4 c-primary-foreground">
-            Primary ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolor similique ipsum obcaecati odit, laboriosam deleniti ea illum qui aperiam.
-        </div>
-        <div class="bg-secondary p4 c-secondary-foreground">
-            Secondary ipsum dolor sit amet consectetur, adipisicing elit. Magnam sit inventore enim placeat tempore illo error minus libero beatae quod.
-        </div>
-        <div class="bg-popover p4 c-popover-foreground">
-            Popover ipsum, dolor sit amet consectetur adipisicing elit. Sequi itaque in voluptate vel quis nihil architecto officia assumenda alias molestias?
-        </div>
-        <div class="bg-muted p4 c-muted-foreground">
-            Muted ipsum dolor sit amet consectetur adipisicing elit. Tempora nihil quam ducimus consequatur voluptatibus iste quia culpa, laudantium nam inventore?
-        </div>
-        <div class="bg-accent p4 c-accent-foreground">
-            Accent ipsum dolor sit amet, consectetur adipisicing elit. Saepe quibusdam dignissimos modi quis reprehenderit voluptatem repudiandae repellendus eligendi sint corporis.
-        </div>
-        <div class="bg-destructive p4 c-destructive-foreground">
-            Destructive ipsum dolor sit amet consectetur adipisicing elit. Hic, excepturi? Vitae voluptate reiciendis provident odio? Soluta sed inventore nihil tenetur.
-        </div>
-        <div class="b b-border p4">
-            Border ipsum dolor sit amet consectetur adipisicing elit. Obcaecati beatae magni tenetur modi voluptates est soluta ad ab a rerum.
-        </div>
-        <div class="p4 ring-1 ring-ring">
-            Ring ipsum dolor, sit amet consectetur adipisicing elit. Dolor, iure molestiae tenetur suscipit fugit consequuntur culpa, saepe, quidem id est fuga modi eum et accusamus necessitatibus earum magnam sapiente delectus!
-        </div>
-        <div class="bg-input p4 c-background">
-            Input ipsum, dolor sit amet consectetur adipisicing elit. Consequatur ipsa, nemo aut architecto provident voluptates labore officia tempore dignissimos excepturi.
-        </div>
-        <div class="col-span-4">
-            <ol class="list-decimal list-inside">
-                <li>Normal ipsum dolor sit amet, consectetur adipisicing elit. Ea, aliquid? Error dolorum pariatur aliquam, numquam sint magnam explicabo. Fugit placeat laboriosam hic et dolore a. Error tenetur doloremque sint fugiat.</li>
-                <li class="c-card-foreground">
-                    Card ipsum dolor sit amet consectetur adipisicing elit. Neque iste deserunt placeat similique dolorum, sint eos voluptas reiciendis autem quia.
-                </li>
-                <li class="c-primary-foreground">
-                    Primary ipsum dolor sit amet, consectetur adipisicing elit. Minima, deserunt placeat? Adipisci debitis impedit fugiat facilis dolor iure autem voluptatum.
-                </li>
-                <li class="c-secondary-foreground">
-                    Secondary ipsum dolor sit amet consectetur, adipisicing elit. Ducimus provident quis illum animi officiis ea consequuntur nostrum quidem quod placeat nemo laboriosam deleniti totam accusantium voluptatem debitis, alias saepe error?
-                </li>
-                <li class="c-muted-foreground">
-                    Muted ipsum dolor sit amet consectetur adipisicing elit. Facilis incidunt laboriosam placeat cum suscipit modi, ex hic voluptate id sunt.
-                </li>
-                <li class="c-accent-foreground">
-                    Accent ipsum dolor sit amet consectetur adipisicing elit. Blanditiis labore ea impedit voluptatibus? Ipsa qui corrupti expedita at sapiente hic?
-                </li>
-                <li class="c-popover-foreground">
-                    Popover, ipsum dolor sit amet consectetur adipisicing elit. Assumenda consequatur necessitatibus, ipsam consectetur quae iste vel alias adipisci. Repudiandae, voluptatibus.
-                </li>
-                <li class="c-destructive-foreground">
-                    Desctructive ipsum dolor sit, amet consectetur adipisicing elit. Laudantium sit illo consequatur similique aut corrupti ad, sequi explicabo deserunt perferendis!
-                </li>
-                <li class="c-input">
-                    Input ipsum dolor sit amet consectetur adipisicing elit. Iste molestiae vero adipisci corrupti cumque! Provident nam est culpa eum ipsum?
-                </li>
-                <li class="c-border">
-                    Border ipsum dolor sit amet consectetur adipisicing elit. Praesentium delectus amet velit fugit ipsum quasi sapiente officiis maiores magni rem.
-                </li>
-                <li class="c-ring">
-                    Ring ipsum dolor sit, amet consectetur adipisicing elit. Doloremque, id maxime! Consectetur eveniet excepturi quae molestias optio. Est, rem voluptatibus?
-                </li>
-            </ol>
-        </div>
+    <DevOnly>
+        <PageProfileDisplay />
+    </DevOnly>
+    <div class="font-mb font-sans lt-md:mx-12 md:(mx-12 w-3xl) lg:w-5xl">
+        <MDC :value="md" class="leading-4.5 [&_i]:vertical-mid" />
     </div>
 </template>
